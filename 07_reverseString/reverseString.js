a712d4a63b2254481f6121fb28c9ab831f40277e
OmarJeghale <omarjeghalef05@gmail.com>
2025-07-08 20:31:10 -0400

    Completed reverseString exercise.

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+let reversed = ''
+const reverseString = function(str) {
+        let splitStr = str.split('')
+        reversed = splitStr.reverse()
+        reversed = reversed.join('')
 
+        return reversed
 };
 
 // Do not edit below this line
