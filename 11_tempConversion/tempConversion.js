3876fe3bdf0ff2b9dcd892dfa4cabd7672c40c45
OmarJeghale <omarjeghalef05@gmail.com>
2025-07-08 20:52:18 -0400

    Completed tempConversion exercise.

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degrees) {
+  const converted = (degrees - 32) * 5/9
+  return Math.round(converted * 10) / 10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degrees) {
+  const converted = (degrees * 9/5 + 32)
+  return Math.round(converted * 10) / 10
 };
 
 // Do not edit below this line
